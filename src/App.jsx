7bfaf0043c0108b5e4619c934533b826733c326d
bhaskar <bassnaidu1242@gmail.com>
2025-03-29 15:52:22 +0530

    footer implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Registrations from "./pages/Registration";
 
@@ -26,6 +27,7 @@ function App() {
           </Routes>
         </main>
       </div>
+      <Footer/>
     </Router>
   );
 }
